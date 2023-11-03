f5ef296f42ce090b0a57a6b778b25af61980c4be
vicentefiorito <vicentefiorito@gmail.com>
2023-11-03 14:20:59 -0400

    tested all methods of the calculator object

--- code/calculator.js
+++ code/calculator.js
@@ -1,4 +1,4 @@
-const calculator = () => {
+export default function calculator() {
     const add = (a,b) => {
         return a + b
     }
@@ -16,9 +16,3 @@ const calculator = () => {
     }
     return {add,substract,divide,multiply}
 }
-
-const calc = calculator()
-console.log(calc.add(3,4))
-console.log(calc.substract(4,3))
-console.log(calc.divide(4,2))
-console.log(calc.multiply(2,2))
